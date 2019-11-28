ce102a2b973a6f3fe74b3f9bb53311331229ad98
antarjot <antarjot74@gmail.com>
2019-11-28 20:55:34 +0530

    Basic Video View with sample movie

--- VideoView/app/src/main/java/com/coffeewithandroid/videoview/MainActivity.kt
+++ VideoView/app/src/main/java/com/coffeewithandroid/videoview/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.coffeewithandroid.videoview
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var playing = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val path = "android.resource://"+ packageName + "/" + R.raw.sample_video
+
+        videoView.setVideoURI(Uri.parse(path))
+
+        videoView.setOnClickListener{
+            when(playing) {
+                true -> {
+                    videoView.pause()
+                    playing = false
+                } else -> {
+                    videoView.start()
+                    playing = true
+                }
+            }
+        }
     }
 }
